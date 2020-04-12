455ca58a634052e9821377b284d2d42a40cf364e
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2020-04-13 01:16:11 +0300

    Configure for Heroku

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -20,15 +20,20 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     services.register(middlewares)
 
     // Create and register a MySQL database config.
+    var databaseConfig: MySQLDatabaseConfig
     var databases = DatabasesConfig()
-    let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
-    let username = Environment.get("DATABASE_USER") ?? "vapor"
-    let password = Environment.get("DATABASE_PASSWORD") ?? "password"
-    let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
-    let databaseConfig = MySQLDatabaseConfig(hostname: hostname,
+    if let url = Environment.get("JAWSDB_URL") {
+        databaseConfig = try! MySQLDatabaseConfig(url: url)!
+    } else {
+        let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
+        let username = Environment.get("DATABASE_USER") ?? "vapor"
+        let password = Environment.get("DATABASE_PASSWORD") ?? "password"
+        let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+        databaseConfig = MySQLDatabaseConfig(hostname: hostname,
                                              username: username,
                                              password: password,
                                              database: databaseName)
+    }
     let database = MySQLDatabase(config: databaseConfig)
     databases.add(database: database, as: .mysql)
     services.register(databases)
